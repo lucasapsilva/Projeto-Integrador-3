b18943a1cc2e2494abbdadc81bd150318decff96
lucasapsilva <lucasapsilva2010@gmail.com>
2013-10-30 14:45:35 -0200

    Create arquivo.h

---
+++ arquivo.h
@@ -0,0 +1,7 @@
+#include "usuario.h"
+#include "produto.h"
+
+void abrir_arquivo(int *QtUsu, int *QtPro);
+FILE *abrir_determinado_arquivo(const char *nome_arquivo, char *Per);
+int fechar_determinado_arquivo(FILE *file);
+long tamanho_arquivo(FILE *stream);
